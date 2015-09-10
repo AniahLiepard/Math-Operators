990dc1eb7552c7acddd92e127f8cccfbeaf17d0b
AniahLiepard <aniah.garcia@techhigh.us>
2015-09-10 09:52:07 -0400

    Math Operators Project
    https://www.thenewboston.com/videos.php?cat=31&amp;video=17973

---
+++ ProjectFive.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ProjectFive {
+	public static void main(String args[]){
+		Scanner pizza = new Scanner(System.in);
+		
+		int raspberries, blackberries, peaches, fruit;
+		
+		raspberries = 24;
+		blackberries = 28;
+		peaches = 18;
+		fruit = raspberries + blackberries + peaches;
+		
+		System.out.println(fruit);
+	}
+}
